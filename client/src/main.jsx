816d9fe416e6be4f6c68fdfe3a2d7663cd3adf21
Jacob <jacobpayne007@gmail.com>
2025-03-26 11:41:43 -0500

    update app to include context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { createRoot } from 'react-dom/client'
+import routes from './routes.jsx'
 import './index.css'
 import App from './App.jsx'
+import { AppProvider } from './AppContext.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+
+const root = createRoot(document.getElementById('root'))
+const router = createBrowserRouter(routes)
+root.render(
+    <AppProvider>
+      <RouterProvider router = {router} />
+    </AppProvider>
+
 )
