fad75b22157cf03c15bc389344a7f30e2dd405aa
Jacob <jacobpayne007@gmail.com>
2025-04-15 21:32:05 -0500

    fix mobile styling

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Nav() {
   return (
-    <nav className='navbar font-body font-semibold text-xl bg-gray text-white'>
+    <nav className='navbar font-body font-semibold text-xl bg-gray text-white '>
         <a href='https://jpayne07.github.io/'><p className='px-3 md:px-5'>&lt; &lt; Back to Portfolio</p></a>
       </nav>
   )
